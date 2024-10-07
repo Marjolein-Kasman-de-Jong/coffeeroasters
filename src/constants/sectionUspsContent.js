88e86c99cd8625ebdae279bc03be3ad06a9239b5
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-10-07 09:37:18 +0200

    Add usps section

---
+++ src/constants/sectionUspsContent.js
@@ -0,0 +1,23 @@
+import coffeeBean from "/home/desktop/icon-coffee-bean.svg";
+import gift from "/home/desktop/icon-gift.svg";
+import truck from "/home/desktop/icon-truck.svg";
+
+const sectionUspsContent = [
+    {
+        title: "Best quality",
+        description: "Discover an endless variety of the world’s best artisan coffee from each of our roasters.",
+        image: coffeeBean
+    },
+    {
+        title: "Exclusive benefits",
+        description: "Special offers and swag when you subscribe, including 30% off your first shipment.",
+        image: gift
+    },
+    {
+        title: "Free shipping",
+        description: "We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.",
+        image: truck
+    }
+]
+
+export default sectionUspsContent;
--- src/sections/usps/Usps.jsx
+++ src/sections/usps/Usps.jsx
@@ -1,8 +1,34 @@
+// Components
+import Card from "../../components/card/Card";
+
+// Constants
+import sectionUspsContent from "../../constants/sectionUspsContent";
+
 // Styles
 import "./usps.css";
 
 export default function Usps() {
   return (
-    <div>Usps</div>
+    <section className="usps">
+      <article>
+        <h2 className="heading-2">
+          Why choose us?
+        </h2>
+        <p className="paragraph-1">
+        A large part of our role is choosing which particular coffees will be featured  in our range. This means working closely with the best coffee growers to give you a more impactful experience on every level.
+        </p>
+      </article>
+      <div className="cards-wrapper">
+        {
+          sectionUspsContent.map((item) => {
+            return <Card 
+              key={item.title} 
+              type="usp" 
+              item={item} 
+            />
+          })
+        }
+      </div>
+    </section>
   )
 }
