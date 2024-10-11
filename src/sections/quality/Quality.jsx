7377001e7cb62fc9bd972c4a91fcd5f1d610562e
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-10-11 09:13:45 +0200

    Add quality section

--- src/sections/quality/Quality.jsx
+++ src/sections/quality/Quality.jsx
@@ -3,6 +3,22 @@ import "./quality.css";
 
 export default function Quality() {
   return (
-    <div>Quality</div>
+    <section className="quality">
+      <article>
+        <picture>
+          <source media="(min-width: 1440px)" srcSet="/about/desktop/image-quality.jpg" />
+          <source media="(min-width: 768px)" srcSet="/about/tablet/image-quality.jpg" />
+          <img src="/about/mobile/image-quality.jpg" alt="Cappucino" />
+        </picture>
+        <div className="text-wrapper">
+          <h2 className="heading-2">
+            Uncompromising quality
+          </h2>
+          <p className="paragraph-1">
+            Although we work with grows who pay close attention to all stages of harvest and processing, we employ, on our end, a rigorous quality control program to avoid over-roasting or baking the coffee dry. Every bag of coffee is tagged with a roast date and batch number. Our goal is to roast consistent, user-friendly coffee, so that brewing is easy and enjoyable.
+          </p>
+        </div>
+      </article>
+    </section>
   )
 }
