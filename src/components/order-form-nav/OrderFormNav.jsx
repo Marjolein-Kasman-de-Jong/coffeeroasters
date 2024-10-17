83bc501429f3200202f3ec4652f664f76faf464f
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-10-17 14:08:35 +0200

    Adjust scrolling behavior

--- src/components/order-form-nav/OrderFormNav.jsx
+++ src/components/order-form-nav/OrderFormNav.jsx
@@ -1,38 +1,42 @@
 // Styles
 import "./order-form-nav.css";
 
+function scrollToSection(id, offset) {
+    const element = document.getElementById(id);
+
+    if (element) {
+        const elementPosition = element.getBoundingClientRect().top + window.scrollY;
+        const offsetPosition = elementPosition - offset;
+
+        window.scrollTo({
+            top: offsetPosition,
+            behavior: "smooth"
+        })
+    }
+}
+
 export default function OrderFormNav() {
     return (
         <nav className="order-form-nav">
             <ol>
-                <li className="nav-item-order-form">
-                    <a href="#sort">
-                        Preferences
-                    </a>
+                <li className="nav-item-order-form" onClick={() => scrollToSection("sort", 110)}>
+                    Preferences
                 </li>
                 <hr />
-                <li className="nav-item-order-form">
-                    <a href="#type">
-                        Bean Type
-                    </a>
+                <li className="nav-item-order-form" onClick={() => scrollToSection("type", 110)}>
+                    Bean Type
                 </li>
                 <hr />
-                <li className="nav-item-order-form">
-                    <a href="#amount">
-                        Quantity
-                    </a>
+                <li className="nav-item-order-form" onClick={() => scrollToSection("amount", 110)}>
+                    Quantity
                 </li>
                 <hr />
-                <li className="nav-item-order-form">
-                    <a href="#grind">
-                        Grind Option
-                    </a>
+                <li className="nav-item-order-form" onClick={() => scrollToSection("grind", 110)}>
+                    Grind Option
                 </li>
                 <hr />
-                <li className="nav-item-order-form">
-                    <a href="#interval">
-                        Deliveries
-                    </a>
+                <li className="nav-item-order-form" onClick={() => scrollToSection("interval", 110)}>
+                    Deliveries
                 </li>
             </ol>
         </nav>
