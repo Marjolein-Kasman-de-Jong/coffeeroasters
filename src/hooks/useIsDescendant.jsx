37fcae3d59f1d69959b83687a285d3ad1d6c9625
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-10-22 08:52:03 +0200

    Add useIsDescendant hook

---
+++ src/hooks/useIsDescendant.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState, useRef } from "react";
+
+function useIsDescendant(targetClass) {
+  const elementRef = useRef(null);
+  const [isDescendant, setIsDescendant] = useState(false);
+
+  useEffect(() => {
+    const element = elementRef.current;
+
+    if (element) {
+      const result = element.closest(`.${targetClass}`);
+      setIsDescendant(!!result);
+    }
+  }, [targetClass]);
+
+  return [elementRef, isDescendant];
+}
+
+export default useIsDescendant;
