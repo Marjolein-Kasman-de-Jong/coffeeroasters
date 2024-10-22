d89656a8412dbec292c89a64c9196baad2d34a49
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-10-22 08:05:41 +0200

    Add page footer content

--- src/sections/page-footer/PageFooter.jsx
+++ src/sections/page-footer/PageFooter.jsx
@@ -1,10 +1,18 @@
+// Components
+import CompanyLogo from "../../components/company-logo/CompanyLogo";
+import NavLinks from "../../components/nav-links/NavLinks";
+
 // Styles
 import "./page-footer.css";
 
 export default function PageFooter() {
   return (
     <footer className="page-footer">
-        PageFooter
+        <div>
+          <CompanyLogo />
+          <NavLinks />
+        </div>
+        socials
     </footer>
   )
 }
