1d2bf7737eab892ea15bfea120e26dca4eac0aa3
Marjolein-Kasman-de-Jong <marjoleinkasmandejong@outlook.com>
2024-10-24 11:56:16 +0200

    Updates

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,12 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-
-// export default defineConfig({
-//   plugins: [react()],
-//   base: '/coffeeroasters/',
-//   build: {
-//     outDir: 'dist',
-//   }
-// });
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { ghPages } from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [react(), ghPages()],
+  plugins: [
+    react(),
+  ],
+  base: '/coffeeroasters/', // Set the base path for GitHub Pages
   build: {
-    outDir: 'dist', // The default output folder
+    outDir: 'dist', // Output directory for the build
   },
-  server: {
-    historyApiFallback: true, // Ensure dev server handles SPA routing
-  },
-  preview: {
-    historyApiFallback: true, // Ensure preview mode handles routing
-  }
 });
